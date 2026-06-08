b1f88c4bc907bbc928e9d147d2ae61df6ada241f
Pochao Wang <dark781228@gmail.com>
2026-06-08 13:13:20 +0800

    新增購物車放棄基礎的 SQL 視圖，計算活躍用戶的放棄產品 ID

---
+++ ecommerce_dbt/models/intermediate/int_cart_abandonment_base.sql
@@ -0,0 +1,18 @@
+{{ config(
+    materialized='view'
+) }}
+
+WITH active_users AS (
+    SELECT DISTINCT user_id
+    FROM {{ ref('stg_orders') }}
+    WHERE order_date >= DATE_SUB((SELECT MAX(order_date) FROM {{ ref('stg_orders') }}), INTERVAL 30 DAY)
+    AND {{ is_valid_order() }}
+)
+
+SELECT
+    user_id,
+    STRING_AGG(DISTINCT CAST(product_id AS STRING) ORDER BY CAST(product_id AS STRING)) AS abandoned_product_ids
+FROM {{ ref('stg_events') }}
+WHERE event_type = 'cart'
+AND user_id NOT IN (SELECT user_id FROM active_users)
+GROUP BY user_id
